912329052b412075cb99c81e34182d85ad8f6c79
Mahesh Govind <vu3mmg@gmail.com>
2016-04-29 12:04:38 +0530

    first Commit

---
+++ client/main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { Meteor } from 'meteor/meteor';
+
+import todoApp from './reducers'
+import App from './components/App'
+
+
+
+
+
+
+let store = createStore(todoApp)
+
+
+Meteor.startup(() => {
+  render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('render-target'));
+});
+
+
+ 
+
+
